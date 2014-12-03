60130b29b1722294f478695dad335b13a26d8546
Abraham <orellanes.abraham@gmail.com>
2014-12-03 15:44:23 -0500

    added calories,protein,carbs,fat to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
       flash[:sucesss] = "Thanks for joining EatHow :)"
   		redirect_to @user
   	else
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
   private
 
   	def user_params
-  		params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  		params.require(:user).permit(:name,:email,:calories,:protein,:carbs,:fat,:password,:password_confirmation)
   	end
 
 end
